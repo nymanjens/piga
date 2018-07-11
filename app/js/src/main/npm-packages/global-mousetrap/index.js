7f3f40ec68f8c679f182524a0af879cc44597cf6
Jens Nyman <nymanjens.nj@gmail.com>
2018-07-11 17:02:49 +0200

    Prevent errors that happen during second but unused load of global-mousetrap

--- app/js/src/main/npm-packages/global-mousetrap/index.js
+++ app/js/src/main/npm-packages/global-mousetrap/index.js
@@ -6,36 +6,42 @@ var Mousetrap = require("mousetrap")
 // (https://github.com/ccampbell/mousetrap/blob/1.6.1/plugins/global-bind/mousetrap-global-bind.js)
 // making it into an npm package.
 
-var _globalCallbacks = {};
-var _originalStopCallback = Mousetrap.prototype.stopCallback;
+if(Mousetrap.prototype) {
+  var _globalCallbacks = {};
+  var _originalStopCallback = Mousetrap.prototype.stopCallback;
 
-Mousetrap.prototype.stopCallback = function(e, element, combo, sequence) {
-  var self = this;
-
-  if (self.paused) {
-    return true;
-  }
-
-  if (_globalCallbacks[combo] || _globalCallbacks[sequence]) {
-    return false;
-  }
-
-  return _originalStopCallback.call(self, e, element, combo);
-};
-
-Mousetrap.init();
-
-module.exports = {
-  bindGlobal: function(keys, callback, action) {
-    Mousetrap.bind(keys, callback, action);
-
-    if (keys instanceof Array) {
-      for (var i = 0; i < keys.length; i++) {
-        _globalCallbacks[keys[i]] = true;
-      }
-      return;
+  Mousetrap.prototype.stopCallback = function(e, element, combo, sequence) {
+    var self = this;
+    if (self.paused) {
+      return true;
     }
+    if (_globalCallbacks[combo] || _globalCallbacks[sequence]) {
+      return false;
+    }
+    return _originalStopCallback.call(self, e, element, combo);
+  };
 
-    _globalCallbacks[keys] = true;
+  Mousetrap.init();
+
+  module.exports = {
+    bindGlobal: function(keys, callback, action) {
+      Mousetrap.bind(keys, callback, action);
+
+      if (keys instanceof Array) {
+        for (var i = 0; i < keys.length; i++) {
+          _globalCallbacks[keys[i]] = true;
+        }
+        return;
+      }
+
+      _globalCallbacks[keys] = true;
+    }
+  };
+} else {
+  module.exports = {
+    bindGlobal: function() {
+      console.log(
+          "Error: Called bindGlobal but Mousetrap was undefined", Mousetrap);
+    }
   }
-};
+}
