3a9d704818cf0eca0553ecebae0dd4652f902db5
Jens Nyman <nymanjens.nj@gmail.com>
2018-08-16 13:48:34 +0200

    Implement TextWithMarkup.toVdomNode

--- app/js/shared/src/main/scala/flux/react/app/desktop/TextWithMarkup.scala
+++ app/js/shared/src/main/scala/flux/react/app/desktop/TextWithMarkup.scala
@@ -1,26 +1,85 @@
 package flux.react.app.desktop
 
+import common.LoggingUtils
 import japgolly.scalajs.react.vdom.html_<^.{VdomNode, _}
 import flux.react.app.desktop.TextWithMarkup.{Formatting, Part}
-import japgolly.scalajs.react.vdom.VdomNode
+import japgolly.scalajs.react.vdom.{VdomArray, VdomNode}
 
 import scala.collection.immutable.Seq
+import scala.collection.mutable
 
 case class TextWithMarkup(parts: List[Part]) {
 
   lazy val contentString: String = parts.map(_.text).mkString
 
   lazy val toVdomNode: VdomNode = {
-    def asVdomNode(x: VdomNode): VdomNode = x
-
-    // TODO: addSpaceAfterTrailingNewline
-    // TODO
-    if (contentString.endsWith("\n")) {
-      // Fix for tailing newline issue. The last \n is seemingly ignored unless a non-empty element is trailing
-      contentString + ' '
-    } else {
-      contentString
+    trait FormattingOption[T] {
+      def getValue(part: Part): T
+      def apply(value: T, children: VdomNode): VdomNode
     }
+    object Bold extends FormattingOption[Boolean] {
+      override def getValue(part: Part): Boolean = part.formatting.bold
+      override def apply(value: Boolean, children: VdomNode): VdomNode =
+        if (value) <.b(children) else children
+    }
+    object Italic extends FormattingOption[Boolean] {
+      override def getValue(part: Part): Boolean = part.formatting.italic
+      override def apply(value: Boolean, children: VdomNode): VdomNode =
+        if (value) <.i(children) else children
+    }
+    object Code extends FormattingOption[Boolean] {
+      override def getValue(part: Part): Boolean = part.formatting.code
+      override def apply(value: Boolean, children: VdomNode): VdomNode =
+        if (value) <.code(children) else children
+    }
+    object Link extends FormattingOption[Option[String]] {
+      override def getValue(part: Part): Option[String] = part.formatting.link
+      override def apply(value: Option[String], children: VdomNode): VdomNode =
+        if (value.isDefined) <.a(^.href := value.get, children) else children
+    }
+
+    def addSpaceAfterTrailingNewline(parts: List[Part]): List[Part] = {
+      if (parts.nonEmpty && parts.last.text.endsWith("\n")) {
+        // Fix for tailing newline issue. The last \n is seemingly ignored unless a non-empty element is trailing
+        parts.updated(parts.length - 1, parts.last + " ")
+      } else {
+        parts
+      }
+    }
+    def toVdomNodeInner(parts: Seq[Part], formattingLeft: List[FormattingOption[_]]): VdomNode = {
+      formattingLeft match {
+        case Nil => parts.map(_.text).mkString
+        case formattingOption :: otherFormattingOptions =>
+          def inner[T](formattingOption: FormattingOption[T]): VdomNode = {
+            var currentFormattingValue: T = null.asInstanceOf[T]
+            val partBuffer = mutable.Buffer[Part]()
+            val resultBuffer = mutable.Buffer[VdomNode]()
+
+            def pushToBuffer(): Unit = {
+              if (partBuffer.nonEmpty) {
+                resultBuffer.append(formattingOption
+                  .apply(currentFormattingValue, toVdomNodeInner(partBuffer.toList, otherFormattingOptions)))
+                partBuffer.clear()
+              }
+            }
+
+            for (part <- parts) {
+              formattingOption.getValue(part) match {
+                case value if value == currentFormattingValue =>
+                  partBuffer.append(part)
+                case newFormattingValue =>
+                  pushToBuffer()
+                  currentFormattingValue = newFormattingValue
+              }
+              pushToBuffer()
+            }
+            resultBuffer.toVdomArray
+          }
+          inner(formattingOption)
+      }
+    }
+
+    toVdomNodeInner(addSpaceAfterTrailingNewline(parts), formattingLeft = List(Link, Code, Italic, Bold))
   }
 
   def +(that: TextWithMarkup): TextWithMarkup = TextWithMarkup(this.parts ++ that.parts)
@@ -53,10 +112,14 @@ object TextWithMarkup {
   def canonicalize(text: TextWithMarkup): TextWithMarkup = text // TODO: Implement
 
   case class Part(text: String, formatting: Formatting = Formatting.none) {
+
     private[TextWithMarkup] def sub(beginOffset: Int, endOffset: Int = -1): Part =
       copy(
         text = if (endOffset == -1) text.substring(beginOffset) else text.substring(beginOffset, endOffset))
+
+    private[TextWithMarkup] def +(thatText: String): Part = copy(text = this.text + thatText)
   }
+
   case class Formatting(bold: Boolean = false,
                         italic: Boolean = false,
                         code: Boolean = false,
