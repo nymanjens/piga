2fabf029da2479c3fd227536bb17df7f08e8e05c
Jens Nyman <nymanjens.nj@gmail.com>
2018-08-24 23:39:02 +0200

    Finish test

--- app/js/client/src/test/scala/flux/react/app/desktop/EditHistoryTest.scala
+++ app/js/client/src/test/scala/flux/react/app/desktop/EditHistoryTest.scala
@@ -40,81 +40,47 @@ object EditHistoryTest extends TestSuite {
       editHistory.undo() ==> None
     }
     "with mergeable edits" - {
-      // TODO
-    }
-    "with no-op edit" - {
-      val abc1 = newTask("ABC")
-      val abc2 = newTask("ABC")
-      val abc3 = newTask("ABC")
-      val xyz = newTask("XYZ")
-      val ghi = newTask("GHI")
-      val klm = newTask("KLM")
+      val abc = newTask("ABC")
+      val abcd = newTask("ABCD")
+      val abcd_ = newTask("ABCD ")
+      val abcd_e = newTask("ABCD E")
+      val abcdx = newTask("ABCDX")
 
-      "very first edit cannot be merged" - {
-        addSimpleEdit(abc1, abc2) // = no-op
-        assertEdit(editHistory.undo(), abc2, abc1)
+      "merged" - {
+        addSimpleEdit(abc, abcd, "D")
+        addSimpleEdit(abcd, abcd_, " ")
+        addSimpleEdit(abcd_, abcd_e, "E")
+
+        assertEdit(editHistory.undo(), abcd_e, abcd)
+        assertEdit(editHistory.undo(), abcd, abc)
       }
+      "not merged after undo()" - {
+        addSimpleEdit(abc, abcd, "D")
+        addSimpleEdit(abcd, abcd_, " ")
+        addSimpleEdit(abcd_, abcd_e, "E")
 
-      "no-op edit is merged" - {
-        "chainable edit" - {
-          addSimpleEdit(xyz, abc1)
-          addSimpleEdit(abc1, abc2) // = no-op
+        assertEdit(editHistory.undo(), abcd_e, abcd)
 
-          assertEdit(editHistory.undo(), abc2, xyz)
-          assertEdit(editHistory.redo(), xyz, abc2)
-        }
-        "unchainable edit" - {
-          addSimpleEdit(xyz, ghi)
-          addSimpleEdit(abc1, abc2) // = no-op
+        addSimpleEdit(abcd, abcdx, "X")
 
-          assertEditSet(editHistory.undo(), Set(ghi, abc2), Set(xyz, abc1))
-        }
-        "partly chainable edit" - {
-          addEdit(Seq(xyz), Seq(ghi, abc1))
-          addSimpleEdit(abc1, abc2) // = no-op
-
-          assertEditSet(editHistory.undo(), Set(ghi, abc2), Set(xyz))
-        }
-      }
-
-      "branching keeps working" - {
-        addSimpleEdit(xyz, abc1)
-        addSimpleEdit(abc1, abc2)
-        addSimpleEdit(abc2, ghi)
-
-        assertEdit(editHistory.undo(), ghi, abc2)
-
-        addSimpleEdit(abc2, abc3) // branch
-
-        editHistory.redo() ==> None
-        assertEdit(editHistory.undo(), klm, abc2)
-        assertEdit(editHistory.undo(), abc2, xyz)
-        editHistory.undo() ==> None
-        assertEdit(editHistory.redo(), xyz, abc2)
-        assertEdit(editHistory.redo(), abc2, klm)
-        editHistory.redo() ==> None
+        assertEdit(editHistory.undo(), abcdx, abcd)
       }
     }
   }
 
   private def addSimpleEdit(removedTask: Task, addedTask: Task, replacementString: String = "")(
-      implicit editHistory: EditHistory): Unit = addEdit(Seq(removedTask), Seq(addedTask), replacementString)
-
-  private def addEdit(removedTasks: Seq[Task], addedTasks: Seq[Task], replacementString: String = "")(
       implicit editHistory: EditHistory): Unit = {
     editHistory.addEdit(
-      removedTasks = removedTasks,
-      addedTasks = addedTasks,
+      removedTasks = Seq(removedTask),
+      addedTasks = Seq(addedTask),
       selectionBeforeEdit = testDetachedSelection,
       selectionAfterEdit = testDetachedSelection,
       replacementString = replacementString
     )
   }
-  private def assertEdit(edit: Option[Edit], removedTask: Task, addedTask: Task): Unit =
-    assertEditSet(edit, Set(removedTask), Set(addedTask))
-  private def assertEditSet(edit: Option[Edit], removedTasks: Set[Task], addedTasks: Set[Task]): Unit = {
-    edit.get.removedTasks.toSet ==> removedTasks
-    edit.get.addedTasks.toSet ==> addedTasks
+  private def assertEdit(edit: Option[Edit], removedTask: Task, addedTask: Task): Unit = {
+    edit.get.removedTasks ==> Seq(removedTask)
+    edit.get.addedTasks ==> Seq(addedTask)
   }
   private def newTask(contentString: String): Task =
     Task.withRandomId(orderToken = orderTokenA, content = TextWithMarkup(contentString), indentation = 1)
