83856f11060f0f68fec3aed50315bdff4663b293
Jens Nyman <nymanjens.nj@gmail.com>
2018-08-26 22:36:27 +0200

    Add documentId to TaskEntity

--- app/shared/src/main/scala/models/document/TaskEntity.scala
+++ app/shared/src/main/scala/models/document/TaskEntity.scala
@@ -3,7 +3,11 @@ package models.document
 import common.OrderToken
 import models.Entity
 
-case class TaskEntity(orderToken: OrderToken, contentHtml: String, indentation: Int, idOption: Option[Long])
+case class TaskEntity(documentId: Long,
+                      contentHtml: String,
+                      orderToken: OrderToken,
+                      indentation: Int,
+                      idOption: Option[Long])
     extends Entity {
 
   override def withId(id: Long) = copy(idOption = Some(id))
