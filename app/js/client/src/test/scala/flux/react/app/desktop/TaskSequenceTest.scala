5a701560163d0cc3fb34a97c916a5ffd804e46d4
Jens Nyman <nymanjens.nj@gmail.com>
2018-08-26 17:01:25 +0200

    Add unit tests

---
+++ app/js/client/src/test/scala/flux/react/app/desktop/TaskSequenceTest.scala
@@ -0,0 +1,49 @@
+package flux.react.app.desktop
+
+import common.testing.JsTestObjects._
+import common.testing.TestModule
+import flux.react.app.desktop.EditHistory.Edit
+import flux.react.app.desktop.TaskSequence.IndexedCursor
+import scala2js.Converters._
+import utest._
+
+import scala.collection.immutable.Seq
+
+object TaskSequenceTest extends TestSuite {
+  val taskBB = newTask("Task BB", orderToken = orderTokenB)
+  val taskEE = newTask("Task EE", orderToken = orderTokenE)
+
+  override def tests = TestSuite {
+    "replaced" - {
+      val taskSequence = createTaskSequence(taskA, taskB, taskC)
+
+      taskSequence.replaced(toReplace = Seq(taskB), toAdd = Seq(taskBB)) ==>
+        createTaskSequence(taskA, taskBB, taskC)
+      taskSequence.replaced(toReplace = Seq(taskB, taskC), toAdd = Seq(taskD, taskE)) ==>
+        createTaskSequence(taskA, taskD, taskE)
+    }
+    "indexOf" - {
+      val taskSequence = createTaskSequence(taskA, taskB, taskBB, taskC, taskD, taskE, taskEE)
+
+      taskSequence.indexOf(taskA) ==> 0
+      taskSequence.indexOf(taskB) ==> 1
+      taskSequence.indexOf(taskBB) ==> 2
+      taskSequence.indexOf(taskC) ==> 3
+      taskSequence.indexOf(taskD) ==> 4
+      taskSequence.indexOf(taskE) ==> 5
+      taskSequence.indexOf(taskEE) ==> 6
+    }
+    "IndexedCursor" - {
+      "plusWord" - {
+        implicit val taskSequence = createTaskSequence(newTask("the red apple"))
+        IndexedCursor(0, 0).plusWord ==> IndexedCursor(0, 3)
+        IndexedCursor(0, 3).plusWord ==> IndexedCursor(0, 7)
+        IndexedCursor(0, 4).plusWord ==> IndexedCursor(0, 7)
+        IndexedCursor(0, 7).plusWord ==> IndexedCursor(0, 13)
+        IndexedCursor(0, 13).plusWord ==> IndexedCursor(0, 13)
+      }
+    }
+  }
+
+  private def createTaskSequence(tasks: Task*): TaskSequence = new TaskSequence(Seq(tasks: _*))
+}
