d7c984eb5b998e333d6a445d958ea5d5d28f7925
Jens Nyman <nymanjens.nj@gmail.com>
2018-09-11 08:57:15 +0200

    DocumentStore syncs local edits to backend

--- app/js/shared/src/main/scala/flux/stores/document/DocumentStore.scala
+++ app/js/shared/src/main/scala/flux/stores/document/DocumentStore.scala
@@ -1,19 +1,28 @@
 package flux.stores.document
 
+import scala.concurrent.duration._
+import scala.scalajs.js
 import flux.stores.StateStore
-import flux.stores.document.DocumentStore.State
+import flux.stores.document.DocumentStore.{Replacement, State, SyncerWithReplenishingDelay}
 import models.access.JsEntityAccess
 import models.document.{Document, Task}
 import models.modification.EntityModification
 
 import scala.collection.immutable.Seq
 import scala.collection.mutable
+import scala.concurrent.Future
+import scala.scalajs.js.timers.SetTimeoutHandle
 
 final class DocumentStore(initialDocument: Document)(implicit entityAccess: JsEntityAccess)
     extends StateStore[State] {
   entityAccess.registerListener(JsEntityAccessListener)
 
   private var _state: State = State(document = initialDocument)
+  private val syncer: SyncerWithReplenishingDelay[Replacement] = new SyncerWithReplenishingDelay(
+    delay = 500.milliseconds,
+    merge = _ merge _,
+    sync = syncReplacement
+  )
 
   // **************** Implementation of StateStore methods **************** //
   override def state: State = _state
@@ -27,9 +36,20 @@ final class DocumentStore(initialDocument: Document)(implicit entityAccess: JsEn
   def replaceTasksWithoutCallingListeners(toReplace: Iterable[Task], toAdd: Iterable[Task]): Document = {
     val newDocument = _state.document.replaced(toReplace, toAdd)
     _state = _state.copy(document = newDocument)
+    syncer.syncWithDelay(Replacement(removedTasks = toReplace.toSet, addedTasks = toAdd.toSet))
     newDocument
   }
 
+  // **************** Private helper methods **************** //
+  private def syncReplacement(replacement: Replacement): Future[_] = {
+    val deletes = replacement.removedTasks.toVector
+      .map(t => EntityModification.createDelete(t.toTaskEntity(_state.document)))
+    val adds = replacement.addedTasks.map(t => EntityModification.Add(t.toTaskEntity(_state.document)))
+
+    entityAccess.persistModifications(deletes ++ adds)
+  }
+
+  // **************** Private inner types **************** //
   private[document] object JsEntityAccessListener extends JsEntityAccess.Listener {
     override def modificationsAddedOrPendingStateChanged(modifications: Seq[EntityModification]): Unit = {
       // TODO
@@ -39,4 +59,37 @@ final class DocumentStore(initialDocument: Document)(implicit entityAccess: JsEn
 
 object DocumentStore {
   case class State(document: Document)
+
+  private class SyncerWithReplenishingDelay[T](delay: FiniteDuration,
+                                               merge: (T, T) => T,
+                                               sync: T => Future[_]) {
+    var currentValue: T = _
+    var timeoutHandle: SetTimeoutHandle = _
+
+    def syncWithDelay(addedValue: T): Unit = {
+      val newValue = currentValue match {
+        case null => addedValue
+        case _    => merge(currentValue, addedValue)
+      }
+      currentValue = newValue
+
+      if (timeoutHandle != null) {
+        js.timers.clearTimeout(timeoutHandle)
+      }
+      timeoutHandle = js.timers.setTimeout(delay) {
+        sync(newValue)
+      }
+    }
+  }
+
+  private case class Replacement(removedTasks: Set[Task], addedTasks: Set[Task]) {
+    def merge(that: Replacement): Replacement = {
+      val overlappingTasks = this.addedTasks intersect that.removedTasks
+
+      Replacement(
+        removedTasks = this.removedTasks ++ that.removedTasks.filterNot(overlappingTasks),
+        addedTasks = that.addedTasks ++ this.addedTasks.filterNot(overlappingTasks),
+      )
+    }
+  }
 }
