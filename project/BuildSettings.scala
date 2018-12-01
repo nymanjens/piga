58b43b7b492f02d0dcd62263539f1c6926b4d234
Jens Nyman <nymanjens.nj@gmail.com>
2018-12-01 15:34:38 +0100

    Remove YAML dependency

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -61,7 +61,6 @@ object BuildSettings {
       "com.typesafe.play" %% "play-cache" % versions.play,
       "com.typesafe.play" %% "play-ws" % versions.play,
       "com.typesafe.play" %% "play-specs2" % versions.play % Test,
-      "org.yaml" % "snakeyaml" % "1.14",
       "com.typesafe.slick" %% "slick" % "3.2.0",
       "com.typesafe.slick" %% "slick-hikaricp" % "3.2.0",
       "commons-lang" % "commons-lang" % "2.6",
