c44bd0545964b1aaeb711c75dca829d7072bcff2
Jens Nyman <nymanjens.nj@gmail.com>
2018-12-28 14:48:42 +0100

    Synchronize StandardConverters

--- app/js/shared/src/main/scala/hydro/scala2js/StandardConverters.scala
+++ app/js/shared/src/main/scala/hydro/scala2js/StandardConverters.scala
@@ -146,6 +146,18 @@ object StandardConverters {
     }
   }
 
+  implicit object FiniteDurationConverter extends Converter[FiniteDuration] {
+
+    private val secondsInDay = 60 * 60 * 24
+
+    override def toJs(duration: FiniteDuration) = {
+      Scala2Js.toJs(duration.toMillis)
+    }
+    override def toScala(value: js.Any) = {
+      Scala2Js.toScala[Long](value).millis
+    }
+  }
+
   implicit object OrderTokenConverter extends Converter[OrderToken] {
     override def toJs(token: OrderToken) = {
       token.parts.toJSArray
