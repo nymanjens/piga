256f320f1e937762dea0fc595af986af1cd092b3
Jens Nyman <nymanjens.nj@gmail.com>
2018-12-29 10:04:09 +0100

    Remove redundant imports

--- app/js/shared/src/main/scala/app/flux/ClientApp.scala
+++ app/js/shared/src/main/scala/app/flux/ClientApp.scala
@@ -5,14 +5,11 @@ import hydro.common.LoggingUtils.logFailure
 import org.scalajs.dom
 import org.scalajs.dom.Event
 import org.scalajs.dom.console
-import org.scalajs.dom.raw.Event
 
 import scala.async.Async.async
 import scala.async.Async.await
 import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
 import scala.scalajs.js
-import scala.scalajs.js.annotation.JSExport
-import scala.scalajs.js.annotation.JSExportTopLevel
 
 object ClientApp {
 
