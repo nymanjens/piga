37ff6a930b43e51b7007dda9d43e212e27e99273
Jens Nyman <nymanjens.nj@gmail.com>
2019-01-03 14:33:28 +0100

    Move EntityModificationPickler to StandardPicklers

--- app/shared/src/main/scala/app/api/Picklers.scala
+++ app/shared/src/main/scala/app/api/Picklers.scala
@@ -2,8 +2,6 @@ package app.api
 
 import app.models.document.DocumentEntity
 import app.models.document.TaskEntity
-import hydro.models.modification.EntityModification
-import hydro.models.modification.EntityType
 import app.models.user.User
 import boopickle.Default._
 import hydro.api.StandardPicklers
@@ -31,51 +29,8 @@ object Picklers extends StandardPicklers {
     }
   }
 
-  implicit val entityPickler = compositePickler[Entity]
+  override implicit val entityPickler: Pickler[Entity] = compositePickler[Entity]
     .addConcreteType[User]
     .addConcreteType[DocumentEntity]
     .addConcreteType[TaskEntity]
-
-  implicit object EntityModificationPickler extends Pickler[EntityModification] {
-    val addNumber = 1
-    val updateNumber = 3
-    val removeNumber = 2
-
-    override def pickle(modification: EntityModification)(implicit state: PickleState): Unit =
-      logExceptions {
-        state.pickle[EntityType.any](modification.entityType)
-        // Pickle number
-        state.pickle(modification match {
-          case _: EntityModification.Add[_]    => addNumber
-          case _: EntityModification.Update[_] => updateNumber
-          case _: EntityModification.Remove[_] => removeNumber
-        })
-        modification match {
-          case EntityModification.Add(entity)      => state.pickle(entity)
-          case EntityModification.Update(entity)   => state.pickle(entity)
-          case EntityModification.Remove(entityId) => state.pickle(entityId)
-        }
-      }
-    override def unpickle(implicit state: UnpickleState): EntityModification = logExceptions {
-      val entityType = state.unpickle[EntityType.any]
-      state.unpickle[Int] match {
-        case `addNumber` =>
-          val entity = state.unpickle[Entity]
-          def addModification[E <: Entity](entity: Entity, entityType: EntityType[E]): EntityModification = {
-            EntityModification.Add(entityType.checkRightType(entity))(entityType)
-          }
-          addModification(entity, entityType)
-        case `updateNumber` =>
-          val entity = state.unpickle[Entity]
-          def updateModification[E <: Entity](entity: Entity,
-                                              entityType: EntityType[E]): EntityModification = {
-            EntityModification.Update(entityType.checkRightType(entity))(entityType)
-          }
-          updateModification(entity, entityType)
-        case `removeNumber` =>
-          val entityId = state.unpickle[Long]
-          EntityModification.Remove(entityId)(entityType)
-      }
-    }
-  }
 }
