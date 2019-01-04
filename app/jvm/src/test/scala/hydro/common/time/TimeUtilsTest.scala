23360f67cf85462584a15860e7e365af3876c263
Jens Nyman <nymanjens.nj@gmail.com>
2019-01-04 23:05:39 +0100

    Synchronize jvm tests

--- app/jvm/src/test/scala/hydro/common/time/TimeUtilsTest.scala
+++ app/jvm/src/test/scala/hydro/common/time/TimeUtilsTest.scala
@@ -1,10 +1,32 @@
 package hydro.common.time
 
+import java.time.LocalDate
 import java.time.Month._
 
 import org.specs2.mutable._
 
 class TimeUtilsTest extends Specification {
+
+  "requireStartOfMonth" in {
+    TimeUtils.requireStartOfMonth(LocalDate.of(1991, APRIL, 1))
+    TimeUtils.requireStartOfMonth(LocalDate.of(1991, APRIL, 3)) must throwA[IllegalArgumentException]
+  }
+
+  "allMonths" in {
+    TimeUtils.allMonths mustEqual Seq(
+      JANUARY,
+      FEBRUARY,
+      MARCH,
+      APRIL,
+      MAY,
+      JUNE,
+      JULY,
+      AUGUST,
+      SEPTEMBER,
+      OCTOBER,
+      NOVEMBER,
+      DECEMBER)
+  }
 
   "parseDateString" in {
     TimeUtils.parseDateString("1992-07-22") mustEqual LocalDateTimes.createDateTime(1992, JULY, 22)
