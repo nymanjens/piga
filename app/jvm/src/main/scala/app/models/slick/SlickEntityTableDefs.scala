nymanjens/facto

[Condensed commit from musik] Support EntityModification.Update with fieldmask

app/jvm/src/main/scala/app/models/slick/SlickEntityTableDefs.scala
@@ -13,6 +13,10 @@ import hydro.models.slick.SlickUtils._
 import hydro.models.slick.SlickUtils.dbApi._
 import hydro.models.slick.SlickUtils.dbApi.{Tag => SlickTag}
 import hydro.models.slick.StandardSlickEntityTableDefs.EntityModificationEntityDef
+import hydro.models.slick.SlickUtils.finiteDurationToMillisMapper
+import hydro.models.slick.SlickUtils.orderTokenToBytesMapper
+import hydro.models.slick.SlickUtils.lastUpdateTimeToBytesMapper
+import hydro.models.UpdatableEntity.LastUpdateTime
 
 import scala.collection.immutable.Seq
 
@@ -40,6 +44,7 @@ object SlickEntityTableDefs {
       def isAdmin = column[Boolean]("isAdmin")
       def expandCashFlowTablesByDefault = column[Boolean]("expandCashFlowTablesByDefault")
       def expandLiquidationTablesByDefault = column[Boolean]("expandLiquidationTablesByDefault")
+      def lastUpdateTime = column[LastUpdateTime]("lastUpdateTime")
 
       override def * =
         (
@@ -49,7 +54,9 @@ object SlickEntityTableDefs {
           isAdmin,
           expandCashFlowTablesByDefault,
           expandLiquidationTablesByDefault,
-          id.?) <> (User.tupled, User.unapply)
+          id.?,
+          lastUpdateTime,
+        ) <> (User.tupled, User.unapply)
     }
   }
 

app/shared/src/main/scala/app/models/access/ModelFields.scala
@@ -5,10 +5,11 @@ import hydro.models.modification.EntityType
 import app.models.money.ExchangeRateMeasurement
 import app.models.user.User
 import hydro.common.time.LocalDateTime
+import hydro.common.CollectionUtils
+import hydro.common.ScalaUtils
 import hydro.models.Entity
 import hydro.models.access.ModelField
 import hydro.models.access.ModelField.IdModelField
-import hydro.models.access.ModelField.toBiMapWithUniqueValues
 
 import scala.collection.immutable.Seq
 
@@ -29,99 +30,148 @@ object ModelFields {
     private type E = User
 
     case object id extends IdModelField[E]
-    case object loginName extends ModelField[String, E]("loginName", _.loginName)
-    case object passwordHash extends ModelField[String, E]("passwordHash", _.passwordHash)
-    case object name extends ModelField[String, E]("name", _.name)
-    case object isAdmin extends ModelField[Boolean, E]("isAdmin", _.isAdmin)
+    case object loginName extends ModelField[String, E]("loginName", _.loginName, v => _.copy(loginName = v))
+    case object passwordHash
+        extends ModelField[String, E]("passwordHash", _.passwordHash, v => _.copy(passwordHash = v))
+    case object name extends ModelField[String, E]("name", _.name, v => _.copy(name = v))
+    case object isAdmin extends ModelField[Boolean, E]("isAdmin", _.isAdmin, v => _.copy(isAdmin = v))
     case object expandCashFlowTablesByDefault
-        extends ModelField[Boolean, E]("expandCashFlowTablesByDefault", _.expandCashFlowTablesByDefault)
+        extends ModelField[Boolean, E](
+          "expandCashFlowTablesByDefault",
+          _.expandCashFlowTablesByDefault,
+          v => _.copy(expandCashFlowTablesByDefault = v))
     case object expandLiquidationTablesByDefault
-        extends ModelField[Boolean, E]("expandLiquidationTablesByDefault", _.expandLiquidationTablesByDefault)
+        extends ModelField[Boolean, E](
+          "expandLiquidationTablesByDefault",
+          _.expandLiquidationTablesByDefault,
+          v => _.copy(expandLiquidationTablesByDefault = v))
   }
 
   object Transaction {
     private type E = app.models.accounting.Transaction
 
     case object id extends IdModelField[E]
-    case object transactionGroupId extends ModelField[Long, E]("transactionGroupId", _.transactionGroupId)
-    case object issuerId extends ModelField[Long, E]("issuerId", _.issuerId)
+    case object transactionGroupId
+        extends ModelField[Long, E](
+          "transactionGroupId",
+          _.transactionGroupId,
+          v => _.copy(transactionGroupId = v))
+    case object issuerId extends ModelField[Long, E]("issuerId", _.issuerId, v => _.copy(issuerId = v))
     case object beneficiaryAccountCode
-        extends ModelField[String, E]("beneficiaryAccountCode", _.beneficiaryAccountCode)
-    case object moneyReservoirCode extends ModelField[String, E]("moneyReservoirCode", _.moneyReservoirCode)
-    case object categoryCode extends ModelField[String, E]("categoryCode", _.categoryCode)
-    case object description extends ModelField[String, E]("description", _.description)
-    case object flowInCents extends ModelField[Long, E]("flowInCents", _.flowInCents)
-    case object detailDescription extends ModelField[String, E]("detailDescription", _.detailDescription)
-    case object tags extends ModelField[Seq[String], E]("tags", _.tags)
-    case object createdDate extends ModelField[LocalDateTime, E]("createdDate", _.createdDate)
-    case object transactionDate extends ModelField[LocalDateTime, E]("transactionDate", _.transactionDate)
-    case object consumedDate extends ModelField[LocalDateTime, E]("consumedDate", _.consumedDate)
+        extends ModelField[String, E](
+          "beneficiaryAccountCode",
+          _.beneficiaryAccountCode,
+          v => _.copy(beneficiaryAccountCode = v))
+    case object moneyReservoirCode
+        extends ModelField[String, E](
+          "moneyReservoirCode",
+          _.moneyReservoirCode,
+          v => _.copy(moneyReservoirCode = v))
+    case object categoryCode
+        extends ModelField[String, E]("categoryCode", _.categoryCode, v => _.copy(categoryCode = v))
+    case object description
+        extends ModelField[String, E]("description", _.description, v => _.copy(description = v))
+    case object flowInCents
+        extends ModelField[Long, E]("flowInCents", _.flowInCents, v => _.copy(flowInCents = v))
+    case object detailDescription
+        extends ModelField[String, E](
+          "detailDescription",
+          _.detailDescription,
+          v => _.copy(detailDescription = v))
+    case object tags extends ModelField[Seq[String], E]("tags", _.tags, v => _.copy(tags = v))
+    case object createdDate
+        extends ModelField[LocalDateTime, E]("createdDate", _.createdDate, v => _.copy(createdDate = v))
+    case object transactionDate
+        extends ModelField[LocalDateTime, E](
+          "transactionDate",
+          _.transactionDate,
+          v => _.copy(transactionDate = v))
+    case object consumedDate
+        extends ModelField[LocalDateTime, E]("consumedDate", _.consumedDate, v => _.copy(consumedDate = v))
   }
 
   object TransactionGroup {
     private type E = app.models.accounting.TransactionGroup
 
     case object id extends IdModelField[E]
-    case object createdDate extends ModelField[LocalDateTime, E]("createdDate", _.createdDate)
+    case object createdDate
+        extends ModelField[LocalDateTime, E]("createdDate", _.createdDate, v => _.copy(createdDate = v))
   }
 
   object BalanceCheck {
     private type E = app.models.accounting.BalanceCheck
 
     case object id extends IdModelField[E]
-    case object issuerId extends ModelField[Long, E]("issuerId", _.issuerId)
-    case object moneyReservoirCode extends ModelField[String, E]("moneyReservoirCode", _.moneyReservoirCode)
-    case object balanceInCents extends ModelField[Long, E]("balanceInCents", _.balanceInCents)
-    case object createdDate extends ModelField[LocalDateTime, E]("createdDate", _.createdDate)
-    case object checkDate extends ModelField[LocalDateTime, E]("checkDate", _.checkDate)
+    case object issuerId extends ModelField[Long, E]("issuerId", _.issuerId, v => _.copy(issuerId = v))
+    case object moneyReservoirCode
+        extends ModelField[String, E](
+          "moneyReservoirCode",
+          _.moneyReservoirCode,
+          v => _.copy(moneyReservoirCode = v))
+    case object balanceInCents
+        extends ModelField[Long, E]("balanceInCents", _.balanceInCents, v => _.copy(balanceInCents = v))
+    case object createdDate
+        extends ModelField[LocalDateTime, E]("createdDate", _.createdDate, v => _.copy(createdDate = v))
+    case object checkDate
+        extends ModelField[LocalDateTime, E]("checkDate", _.checkDate, v => _.copy(checkDate = v))
   }
 
   object ExchangeRateMeasurement {
     private type E = ExchangeRateMeasurement
 
     case object id extends IdModelField[E]
-    case object date extends ModelField[LocalDateTime, E]("date", _.date)
+    case object date extends ModelField[LocalDateTime, E]("date", _.date, v => _.copy(date = v))
     case object foreignCurrencyCode
-        extends ModelField[String, E]("foreignCurrencyCode", _.foreignCurrencyCode)
+        extends ModelField[String, E](
+          "foreignCurrencyCode",
+          _.foreignCurrencyCode,
+          v => _.copy(foreignCurrencyCode = v))
     case object ratioReferenceToForeignCurrency
-        extends ModelField[Double, E]("ratioReferenceToForeignCurrency", _.ratioReferenceToForeignCurrency)
+        extends ModelField[Double, E](
+          "ratioReferenceToForeignCurrency",
+          _.ratioReferenceToForeignCurrency,
+          v => _.copy(ratioReferenceToForeignCurrency = v))
   }
 
   // **************** Field numbers **************** //
   private val fieldToNumberMap: ImmutableBiMap[ModelField[_, _], Int] =
-    toBiMapWithUniqueValues(
-      User.id,
-      User.loginName,
-      User.passwordHash,
-      User.name,
-      User.isAdmin,
-      User.expandCashFlowTablesByDefault,
-      User.expandLiquidationTablesByDefault,
-      Transaction.id,
-      Transaction.transactionGroupId,
-      Transaction.issuerId,
-      Transaction.beneficiaryAccountCode,
-      Transaction.moneyReservoirCode,
-      Transaction.categoryCode,
-      Transaction.description,
-      Transaction.flowInCents,
-      Transaction.detailDescription,
-      Transaction.tags,
-      Transaction.createdDate,
-      Transaction.transactionDate,
-      Transaction.consumedDate,
-      TransactionGroup.id,
-      TransactionGroup.createdDate,
-      BalanceCheck.id,
-      BalanceCheck.issuerId,
-      BalanceCheck.moneyReservoirCode,
-      BalanceCheck.balanceInCents,
-      BalanceCheck.createdDate,
-      BalanceCheck.checkDate,
-      ExchangeRateMeasurement.id,
-      ExchangeRateMeasurement.date,
-      ExchangeRateMeasurement.foreignCurrencyCode,
-      ExchangeRateMeasurement.ratioReferenceToForeignCurrency
+    CollectionUtils.toBiMapWithStableIntKeys(
+      stableNameMapper = field =>
+        ScalaUtils.stripRequiredPrefix(field.getClass.getName, prefix = ModelFields.getClass.getName),
+      values = Seq(
+        User.id,
+        User.loginName,
+        User.passwordHash,
+        User.name,
+        User.isAdmin,
+        User.expandCashFlowTablesByDefault,
+        User.expandLiquidationTablesByDefault,
+        Transaction.id,
+        Transaction.transactionGroupId,
+        Transaction.issuerId,
+        Transaction.beneficiaryAccountCode,
+        Transaction.moneyReservoirCode,
+        Transaction.categoryCode,
+        Transaction.description,
+        Transaction.flowInCents,
+        Transaction.detailDescription,
+        Transaction.tags,
+        Transaction.createdDate,
+        Transaction.transactionDate,
+        Transaction.consumedDate,
+        TransactionGroup.id,
+        TransactionGroup.createdDate,
+        BalanceCheck.id,
+        BalanceCheck.issuerId,
+        BalanceCheck.moneyReservoirCode,
+        BalanceCheck.balanceInCents,
+        BalanceCheck.createdDate,
+        BalanceCheck.checkDate,
+        ExchangeRateMeasurement.id,
+        ExchangeRateMeasurement.date,
+        ExchangeRateMeasurement.foreignCurrencyCode,
+        ExchangeRateMeasurement.ratioReferenceToForeignCurrency
+      )
     )
 
   def toNumber(field: ModelField[_, _]): Int = fieldToNumberMap.get(field)

nymanjens/piga

app/jvm/src/main/scala/app/models/slick/SlickEntityTableDefs.scala
@@ -1,24 +1,20 @@
 package app.models.slick
 
-import java.nio.ByteBuffer
-import java.time.Instant
-
-import app.api.Picklers._
 import app.models.document.DocumentEntity
 import app.models.document.TaskEntity
 import app.models.user.User
-import boopickle.Default.Pickle
-import boopickle.Default.Unpickle
 import hydro.common.OrderToken
 import hydro.common.time.LocalDateTime
 import hydro.common.Tags
-import hydro.models.modification.EntityModification
-import hydro.models.modification.EntityModificationEntity
 import hydro.models.slick.SlickEntityTableDef
 import hydro.models.slick.SlickEntityTableDef.EntityTable
 import hydro.models.slick.SlickUtils._
 import hydro.models.slick.SlickUtils.dbApi._
 import hydro.models.slick.SlickUtils.dbApi.{Tag => SlickTag}
+import hydro.models.slick.SlickUtils.lastUpdateTimeToBytesMapper
+import hydro.models.slick.SlickUtils.orderTokenToBytesMapper
+import hydro.models.UpdatableEntity.LastUpdateTime
+import hydro.models.slick.StandardSlickEntityTableDefs.EntityModificationEntityDef
 
 import scala.collection.immutable.Seq
 
@@ -38,9 +34,10 @@ object SlickEntityTableDefs {
       def passwordHash = column[String]("passwordHash")
       def name = column[String]("name")
       def isAdmin = column[Boolean]("isAdmin")
+      def lastUpdateTime = column[LastUpdateTime]("lastUpdateTime")
 
       override def * =
-        (loginName, passwordHash, name, isAdmin, id.?) <> (User.tupled, User.unapply)
+        (loginName, passwordHash, name, isAdmin, id.?, lastUpdateTime) <> (User.tupled, User.unapply)
     }
   }
 
@@ -80,57 +77,6 @@ object SlickEntityTableDefs {
 
       override def * =
         (documentId, contentHtml, orderToken, indentation, collapsed, delayedUntil, tags, id.?) <> (TaskEntity.tupled, TaskEntity.unapply)
-    }
-  }
-
-  implicit object EntityModificationEntityDef extends SlickEntityTableDef[EntityModificationEntity] {
-
-    override val tableName: String = "ENTITY_MODIFICATION_ENTITY"
-    override def table(tag: SlickTag): Table = new Table(tag)
-
-    /* override */
-    final class Table(tag: SlickTag) extends EntityTable[EntityModificationEntity](tag, tableName) {
-      def userId = column[Long]("userId")
-      def entityId = column[Long]("entityId")
-      def change = column[EntityModification]("modification")
-      def instant = column[Instant]("date")
-      // The instant field can't hold the nano precision of the `instant` field above. It thus
-      // has to be persisted separately.
-      def instantNanos = column[Long]("instantNanos")
-
-      override def * = {
-        def tupled(
-            tuple: (Long, Long, EntityModification, Instant, Long, Option[Long])): EntityModificationEntity =
-          tuple match {
-            case (userId, entityId, modification, instant, instantNanos, idOption) =>
-              EntityModificationEntity(
-                userId = userId,
-                modification = modification,
-                instant = Instant.ofEpochSecond(instant.getEpochSecond, instantNanos),
-                idOption = idOption
-              )
-          }
-        def unapply(e: EntityModificationEntity)
-          : Option[(Long, Long, EntityModification, Instant, Long, Option[Long])] =
-          Some((e.userId, e.modification.entityId, e.modification, e.instant, e.instant.getNano, e.idOption))
-
-        (userId, entityId, change, instant, instantNanos, id.?) <> (tupled _, unapply _)
-      }
-    }
-
-    implicit val entityModificationToBytesMapper: ColumnType[EntityModification] = {
-      def toBytes(modification: EntityModification) = {
-        val byteBuffer = Pickle.intoBytes(modification)
-
-        val byteArray = new Array[Byte](byteBuffer.remaining)
-        byteBuffer.get(byteArray)
-        byteArray
-      }
-      def toEntityModification(bytes: Array[Byte]) = {
-        val byteBuffer = ByteBuffer.wrap(bytes)
-        Unpickle[EntityModification].fromBytes(byteBuffer)
-      }
-      MappedColumnType.base[EntityModification, Array[Byte]](toBytes, toEntityModification)
     }
   }
 }

app/shared/src/main/scala/app/models/access/ModelFields.scala
@@ -1,16 +1,17 @@
 package app.models.access
 
-import hydro.common.GuavaReplacement.ImmutableBiMap
-import hydro.common.OrderToken
 import app.models.document.DocumentEntity
 import app.models.document.TaskEntity
-import hydro.models.modification.EntityType
 import app.models.user.User
+import hydro.common.GuavaReplacement.ImmutableBiMap
+import hydro.common.OrderToken
 import hydro.common.time.LocalDateTime
+import hydro.common.CollectionUtils
+import hydro.common.ScalaUtils
+import hydro.models.modification.EntityType
 import hydro.models.Entity
 import hydro.models.access.ModelField
 import hydro.models.access.ModelField.IdModelField
-import hydro.models.access.ModelField.toBiMapWithUniqueValues
 
 import scala.collection.immutable.Seq
 
@@ -27,52 +28,66 @@ object ModelFields {
     private type E = User
 
     case object id extends IdModelField[E]
-    case object loginName extends ModelField[String, E]("loginName", _.loginName)
-    case object passwordHash extends ModelField[String, E]("passwordHash", _.passwordHash)
-    case object name extends ModelField[String, E]("name", _.name)
-    case object isAdmin extends ModelField[Boolean, E]("isAdmin", _.isAdmin)
+    case object loginName extends ModelField[String, E]("loginName", _.loginName, v => _.copy(loginName = v))
+    case object passwordHash
+        extends ModelField[String, E]("passwordHash", _.passwordHash, v => _.copy(passwordHash = v))
+    case object name extends ModelField[String, E]("name", _.name, v => _.copy(name = v))
+    case object isAdmin extends ModelField[Boolean, E]("isAdmin", _.isAdmin, v => _.copy(isAdmin = v))
   }
 
   object DocumentEntity {
     private type E = DocumentEntity
 
     case object id extends IdModelField[E]
-    case object name extends ModelField[String, E]("name", _.name)
-    case object orderToken extends ModelField[OrderToken, E]("orderToken", _.orderToken)
+    case object name extends ModelField[String, E]("name", _.name, v => _.copy(name = v))
+    case object orderToken
+        extends ModelField[OrderToken, E]("orderToken", _.orderToken, v => _.copy(orderToken = v))
   }
 
   object TaskEntity {
     private type E = TaskEntity
 
     case object id extends IdModelField[E]
-    case object documentId extends ModelField[Long, E]("documentId", _.documentId)
-    case object contentHtml extends ModelField[String, E]("contentHtml", _.contentHtml)
-    case object orderToken extends ModelField[OrderToken, E]("orderToken", _.orderToken)
-    case object indentation extends ModelField[Int, E]("indentation", _.indentation)
-    case object collapsed extends ModelField[Boolean, E]("collapsed", _.collapsed)
-    case object delayedUntil extends ModelField[Option[LocalDateTime], E]("delayedUntil", _.delayedUntil)
-    case object tags extends ModelField[Seq[String], E]("tags", _.tags)
+    case object documentId
+        extends ModelField[Long, E]("documentId", _.documentId, v => _.copy(documentId = v))
+    case object contentHtml
+        extends ModelField[String, E]("contentHtml", _.contentHtml, v => _.copy(contentHtml = v))
+    case object orderToken
+        extends ModelField[OrderToken, E]("orderToken", _.orderToken, v => _.copy(orderToken = v))
+    case object indentation
+        extends ModelField[Int, E]("indentation", _.indentation, v => _.copy(indentation = v))
+    case object collapsed extends ModelField[Boolean, E]("collapsed", _.collapsed, v => _.copy(collapsed = v))
+    case object delayedUntil
+        extends ModelField[Option[LocalDateTime], E](
+          "delayedUntil",
+          _.delayedUntil,
+          v => _.copy(delayedUntil = v))
+    case object tags extends ModelField[Seq[String], E]("tags", _.tags, v => _.copy(tags = v))
   }
 
   // **************** Field numbers **************** //
-  private val fieldToNumberMap: ImmutableBiMap[ModelField[_, _], Int] =
-    toBiMapWithUniqueValues(
-      User.id,
-      User.loginName,
-      User.passwordHash,
-      User.name,
-      User.isAdmin,
-      DocumentEntity.id,
-      DocumentEntity.name,
-      DocumentEntity.orderToken,
-      TaskEntity.id,
-      TaskEntity.documentId,
-      TaskEntity.contentHtml,
-      TaskEntity.orderToken,
-      TaskEntity.indentation,
-      TaskEntity.collapsed,
-      TaskEntity.delayedUntil,
-      TaskEntity.tags,
+  private val fieldToNumberMap: ImmutableBiMap[ModelField.any, Int] =
+    CollectionUtils.toBiMapWithStableIntKeys(
+      stableNameMapper = field =>
+        ScalaUtils.stripRequiredPrefix(field.getClass.getName, prefix = ModelFields.getClass.getName),
+      values = Seq(
+        User.id,
+        User.loginName,
+        User.passwordHash,
+        User.name,
+        User.isAdmin,
+        DocumentEntity.id,
+        DocumentEntity.name,
+        DocumentEntity.orderToken,
+        TaskEntity.id,
+        TaskEntity.documentId,
+        TaskEntity.contentHtml,
+        TaskEntity.orderToken,
+        TaskEntity.indentation,
+        TaskEntity.collapsed,
+        TaskEntity.delayedUntil,
+        TaskEntity.tags,
+      )
     )
   def toNumber(field: ModelField[_, _]): Int = fieldToNumberMap.get(field)
   def fromNumber(number: Int): ModelField[_, _] = fieldToNumberMap.inverse().get(number)