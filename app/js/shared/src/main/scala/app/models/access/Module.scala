d7308b08bbafff122b42193c43f428267c2e53ed
Jens Nyman <nymanjens.nj@gmail.com>
2019-04-06 23:33:51 +0200

    Disable separateDbPerCollection

--- app/js/shared/src/main/scala/app/models/access/Module.scala
+++ app/js/shared/src/main/scala/app/models/access/Module.scala
@@ -25,7 +25,7 @@ final class Module(implicit user: User,
   implicit val entityAccess: JsEntityAccess = {
     val webWorkerModule = new hydro.models.access.webworker.Module()
     implicit val localDatabaseWebWorkerApiStub = webWorkerModule.localDatabaseWebWorkerApiStub
-    val localDatabaseFuture = LocalDatabaseImpl.create(separateDbPerCollection = true)
+    val localDatabaseFuture = LocalDatabaseImpl.create(separateDbPerCollection = false)
     implicit val remoteDatabaseProxy = HybridRemoteDatabaseProxy.create(localDatabaseFuture)
     val entityAccess = new JsEntityAccessImpl()
 
