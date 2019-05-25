8d3e9e74b31c572b277f6b64aa7d0aacb349b85f
Jens Nyman <nymanjens.nj@gmail.com>
2019-05-25 22:42:05 +0200

    Manually bump version for testing purposes

--- app/shared/src/main/scala/app/AppVersion.scala
+++ app/shared/src/main/scala/app/AppVersion.scala
@@ -2,5 +2,5 @@ package app
 
 /** Version string that gets incremented on every deploy. */
 object AppVersion {
-  val versionString: String = "1.02.x02"
+  val versionString: String = "1.02.x03"
 }
