94e0f48b9bc005258203755c25102990bf142d72
Jens Nyman <nymanjens.nj@gmail.com>
2020-02-04 09:34:29 +0100

    Make model changes

--- app/shared/src/main/scala/app/models/document/AccessibleDocumentEntity.scala
+++ app/shared/src/main/scala/app/models/document/AccessibleDocumentEntity.scala
@@ -1,22 +1,24 @@
 package app.models.document
 
 import hydro.common.OrderToken
-import hydro.models.modification.EntityType
 import hydro.models.Entity
 import hydro.models.UpdatableEntity
 import hydro.models.UpdatableEntity.LastUpdateTime
 
-case class DocumentEntity(
-    name: String,
+case class AccessibleDocumentEntity(
+    documentId: Long,
+    userId: Long,
+    orderToken: OrderToken,
     override val idOption: Option[Long] = None,
     override val lastUpdateTime: LastUpdateTime = LastUpdateTime.neverUpdated,
-) extends UpdatableEntity {
+) extends UpdatableEntity
+    with Ordered[AccessibleDocumentEntity] {
 
   override def withId(id: Long) = copy(idOption = Some(id))
   override def withLastUpdateTime(time: LastUpdateTime): Entity = copy(lastUpdateTime = time)
-}
-object DocumentEntity {
-  implicit val Type: EntityType[DocumentEntity] = EntityType()
 
-  def tupled = (this.apply _).tupled
+  // **************** Ordered methods **************** //
+  override def compare(that: AccessibleDocumentEntity): Int = {
+    this.orderToken compare that.orderToken
+  }
 }
