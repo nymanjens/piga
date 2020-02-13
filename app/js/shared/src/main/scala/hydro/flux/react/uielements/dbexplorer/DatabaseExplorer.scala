dbf6e6c67f9b42ba25a21067440a28bebdad47b4
Jens Nyman <nymanjens.nj@gmail.com>
2020-02-13 22:52:49 +0100

    Convert DatabaseExplorer to a HydroReactComponent

--- app/js/shared/src/main/scala/hydro/flux/react/uielements/dbexplorer/DatabaseExplorer.scala
+++ app/js/shared/src/main/scala/hydro/flux/react/uielements/dbexplorer/DatabaseExplorer.scala
@@ -3,28 +3,36 @@ package hydro.flux.react.uielements.dbexplorer
 import hydro.common.I18n
 import hydro.flux.react.uielements.Bootstrap
 import hydro.flux.react.uielements.PageHeader
+import hydro.flux.react.HydroReactComponent
 import hydro.flux.router.RouterContext
 import japgolly.scalajs.react._
+import japgolly.scalajs.react.vdom.html_<^
 import japgolly.scalajs.react.vdom.html_<^._
 
-final class DatabaseExplorer(implicit i18n: I18n, pageHeader: PageHeader) {
+import scala.collection.immutable.Seq
 
-  private val component = ScalaComponent
-    .builder[Props](getClass.getSimpleName)
-    .renderP(($, props) => {
-      implicit val router = props.router
-      <.span(
-        pageHeader(router.currentPage),
-        Bootstrap.Row("Hello world")
-      )
-    })
-    .build
+final class DatabaseExplorer(implicit i18n: I18n, pageHeader: PageHeader)
+    extends HydroReactComponent.Stateless {
 
   // **************** API ****************//
   def apply(router: RouterContext): VdomElement = {
     component(Props(router))
   }
 
-  // **************** Private inner types ****************//
-  private case class Props(router: RouterContext)
+  // **************** Implementation of HydroReactComponent methods ****************//
+  override protected val statelessConfig = StatelessComponentConfig(backendConstructor = new Backend(_))
+
+  // **************** Implementation of HydroReactComponent types ****************//
+  protected case class Props(router: RouterContext)
+
+  protected final class Backend(val $ : BackendScope[Props, State]) extends BackendBase($) {
+    override def render(props: Props, state: Unit): VdomElement = {
+      implicit val router = props.router
+
+      <.span(
+        pageHeader(router.currentPage),
+        Bootstrap.Row("Hello world")
+      )
+    }
+  }
 }
