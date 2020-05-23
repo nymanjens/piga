5519038906153a2e41e70392ccdda46f98fc438e
Jens Nyman <nymanjens.nj@gmail.com>
2020-05-23 18:47:17 +0200

    Avoid the mobile version to sleep the window

--- app/js/shared/src/main/scala/app/flux/ClientApp.scala
+++ app/js/shared/src/main/scala/app/flux/ClientApp.scala
@@ -1,5 +1,6 @@
 package app.flux
 
+import hydro.common.BrowserUtils
 import hydro.common.JsLoggingUtils.logExceptions
 import hydro.common.JsLoggingUtils.logFailure
 import org.scalajs.dom
@@ -19,6 +20,7 @@ object ClientApp {
     //GlobalStyles.addToDocument()
 
     setUpServiceWorker()
+    setUpNoSleep()
 
     val commonTimeModule = new hydro.common.time.Module
     implicit val clock = commonTimeModule.clock
@@ -44,6 +46,58 @@ object ClientApp {
           (registration: Any) => {},
           (err: Any) => println(s"  Installation of service worker failed: ${err}"),
         )
+    }
+  }
+
+
+  // Snippet copied from https://stackoverflow.com/a/46363553
+  private def setUpNoSleep(): Unit = logExceptions {
+    if (BrowserUtils.isMobileOrTablet) {
+      def base64(mimeType: String, base64: String): String = {
+        "data:" + mimeType + ";base64," + base64
+      }
+
+      var video = dom.document.createElement("video").asInstanceOf[dom.raw.HTMLVideoElement]
+      video.setAttribute("loop", "")
+
+      def addSourceToVideo(element: dom.raw.Element, tpe: String, dataURI: String) = {
+        val source = dom.document.createElement("source").asInstanceOf[js.Dynamic]
+        source.src = dataURI
+        source.tpe = "video/" + tpe
+        element.appendChild(source.asInstanceOf[dom.raw.Node])
+      }
+
+      addSourceToVideo(
+        video,
+        "webm",
+        base64(
+          "video/webm",
+          "GkXfo0AgQoaBAUL3gQFC8oEEQvOBCEKCQAR3ZWJtQoeBAkKFgQIYU4BnQI0VSalmQCgq17FAAw9CQE2AQAZ3aGFtbXlXQUAGd" +
+            "2hhbW15RIlACECPQAAAAAAAFlSua0AxrkAu14EBY8WBAZyBACK1nEADdW5khkAFVl9WUDglhohAA1ZQOIOBAeBABrCBCLqB" +
+            "CB9DtnVAIueBAKNAHIEAAIAwAQCdASoIAAgAAUAmJaQAA3AA/vz0AAA="
+        )
+      )
+      addSourceToVideo(
+        video,
+        "mp4",
+        base64(
+          "video/mp4",
+          "AAAAHGZ0eXBpc29tAAACAGlzb21pc28ybXA0MQAAAAhmcmVlAAAAG21kYXQAAAGzABAHAAABthADAowdbb9/AAAC6W1vb3YAA" +
+            "ABsbXZoZAAAAAB8JbCAfCWwgAAAA+gAAAAAAAEAAAEAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAA" +
+            "AAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIAAAIVdHJhawAAAFx0a2hkAAAAD3wlsIB8JbCAAAAAAQAAAAA" +
+            "AAAAAAAAAAAAAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAQAAAAAAIAAAACAAAAAABsW1kaW" +
+            "EAAAAgbWRoZAAAAAB8JbCAfCWwgAAAA+gAAAAAVcQAAAAAAC1oZGxyAAAAAAAAAAB2aWRlAAAAAAAAAAAAAAAAVmlkZW9IY" +
+            "W5kbGVyAAAAAVxtaW5mAAAAFHZtaGQAAAABAAAAAAAAAAAAAAAkZGluZgAAABxkcmVmAAAAAAAAAAEAAAAMdXJsIAAAAAEA" +
+            "AAEcc3RibAAAALhzdHNkAAAAAAAAAAEAAACobXA0dgAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAAAIAAgASAAAAEgAAAAAAAA" +
+            "AAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABj//wAAAFJlc2RzAAAAAANEAAEABDwgEQAAAAADDUAAAAAABS" +
+            "0AAAGwAQAAAbWJEwAAAQAAAAEgAMSNiB9FAEQBFGMAAAGyTGF2YzUyLjg3LjQGAQIAAAAYc3R0cwAAAAAAAAABAAAAAQAAA" +
+            "AAAAAAcc3RzYwAAAAAAAAABAAAAAQAAAAEAAAABAAAAFHN0c3oAAAAAAAAAEwAAAAEAAAAUc3RjbwAAAAAAAAABAAAALAAA" +
+            "AGB1ZHRhAAAAWG1ldGEAAAAAAAAAIWhkbHIAAAAAAAAAAG1kaXJhcHBsAAAAAAAAAAAAAAAAK2lsc3QAAAAjqXRvbwAAABt" +
+            "kYXRhAAAAAQAAAABMYXZmNTIuNzguMw=="
+        )
+      )
+
+      video.play()
     }
   }
 }
