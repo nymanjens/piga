ac59c8c4811ea8c34af0849843d73abb68613273
Jens Nyman <nymanjens.nj@gmail.com>
2021-03-02 15:51:02 +0100

    TestParameterInjector: Proof of concept

--- app/jvm/src/test/scala/app/common/CaseFormatsTest.scala
+++ app/jvm/src/test/scala/app/common/CaseFormatsTest.scala
@@ -1,15 +1,30 @@
 package app.common
 
+import scala.collection.JavaConverters._
 import com.google.common.truth.Truth.assertThat
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 import org.junit.Test
+import com.google.testing.junit.testparameterinjector.TestParameterInjector
+import com.google.testing.junit.testparameterinjector.TestParameter
+import com.google.testing.junit.testparameterinjector.TestParameters
 
-@RunWith(classOf[JUnit4])
-class CaseFormatsTest  {
+@RunWith(classOf[TestParameterInjector])
+class CaseFormatsTest {
 
   @Test
-  def myTest() = {
-    assertThat(CaseFormats.tokenize("ab_c")) isEqualTo Seq("ab", "c")
+  @TestParameters(
+    Array(
+      "{input: ab_c, expected: [ab, c]}",
+      "{input: abC, expected: [ab, c]}",
+    )
+  )
+  def tokenize_success(input: String, expected: java.util.List[String]) = {
+    assertThat(CaseFormats.tokenize(input).asJava) containsExactlyElementsIn expected
+  }
+
+  @Test
+  def testMethod(@TestParameter(Array("A", "B")) variable: String) = {
+    assertThat(variable) isEqualTo "B"
   }
 }
