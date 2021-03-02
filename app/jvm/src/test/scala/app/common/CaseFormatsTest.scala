757a31d3fb5d8634fae338bf8e801a6b6b42e547
Jens Nyman <nymanjens.nj@gmail.com>
2021-03-02 15:56:57 +0100

    Add all tests

--- app/jvm/src/test/scala/app/common/CaseFormatsTest.scala
+++ app/jvm/src/test/scala/app/common/CaseFormatsTest.scala
@@ -1,13 +1,13 @@
 package app.common
 
-import scala.collection.JavaConverters._
 import com.google.common.truth.Truth.assertThat
-import org.junit.runner.RunWith
-import org.junit.runners.JUnit4
-import org.junit.Test
-import com.google.testing.junit.testparameterinjector.TestParameterInjector
 import com.google.testing.junit.testparameterinjector.TestParameter
+import com.google.testing.junit.testparameterinjector.TestParameterInjector
 import com.google.testing.junit.testparameterinjector.TestParameters
+import org.junit.runner.RunWith
+import org.junit.Test
+
+import scala.collection.JavaConverters._
 
 @RunWith(classOf[TestParameterInjector])
 class CaseFormatsTest {
@@ -15,16 +15,21 @@ class CaseFormatsTest {
   @Test
   @TestParameters(
     Array(
+      // snake_case
       "{input: ab_c, expected: [ab, c]}",
+      // CamelCase
       "{input: abC, expected: [ab, c]}",
+      "{input: AbCdE, expected: [ab, cd, e]}",
+      "{input: AbCdEF, expected: [ab, cd, e, f]}",
+      // dash-case
+      "{input: ab-C, expected: [ab, c]}",
+      // Other
+      "{input: THE angry Bird, expected: [the, angry, bird]}",
+      "{input: 'a;b,c:-=+', expected: [a, b, c]}",
+      "{input: a1, expected: [a1]}",
     )
   )
   def tokenize_success(input: String, expected: java.util.List[String]) = {
     assertThat(CaseFormats.tokenize(input).asJava) containsExactlyElementsIn expected
-  }
-
-  @Test
-  def testMethod(@TestParameter(Array("A", "B")) variable: String) = {
-    assertThat(variable) isEqualTo "B"
   }
 }
