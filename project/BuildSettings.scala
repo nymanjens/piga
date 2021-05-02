bbefdd751ad66ceef6e1bf5e5d3b36574b833dce
Jens Nyman <nymanjens.nj@gmail.com>
2021-05-02 21:20:14 +0200

    Add missing dependency

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -63,6 +63,7 @@ object BuildSettings {
       "com.typesafe.play" %% "play-cache" % versions.play,
       "com.typesafe.play" %% "play-ws" % versions.play,
       "com.typesafe.play" %% "play-specs2" % versions.play % Test,
+      "org.yaml" % "snakeyaml" % "1.14",
       "com.google.truth" % "truth" % "1.1.2" % Test,
       "com.google.testparameterinjector" % "test-parameter-injector" % "1.0-rc1" % Test,
       "com.typesafe.slick" %% "slick" % "3.2.0",
