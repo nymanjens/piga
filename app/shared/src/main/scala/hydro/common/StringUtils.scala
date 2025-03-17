b8bbc16001386506117033d3dc5d852f9f260d47
Jens Nyman <nymanjens.nj@gmail.com>
2025-03-17 10:49:36 +0100

    Rewrite StringUtils without regex

--- app/shared/src/main/scala/hydro/common/StringUtils.scala
+++ app/shared/src/main/scala/hydro/common/StringUtils.scala
@@ -1,65 +1,61 @@
 package hydro.common
 
 import java.nio.charset.Charset
+import java.util.stream.Collectors
+import scala.collection.JavaConverters._
 
 object StringUtils {
 
-  private val EDGE_CASE_REPLACEMENTS: GuavaReplacement.ImmutableBiMap[Char, String] =
-    GuavaReplacement.ImmutableBiMap
-      .builder()
-      // Workaround for odd bug in p{Z} regex below that also seems to be filtering out newlines
-      .put('\n', "%)A_[7|%*&")
-      // Supported by latin1, but with a different code
-      .put('€', "%)B_[7|%*&")
-      // Supported by latin1, but with a different code
-      .put('‰', "%)C_[7|%*&")
-      .build()
+  private final val INVISIBLE_CHARACTER_TYPES: Set[Int] =
+    Set(
+      Character.CONTROL,
+      Character.FORMAT,
+      Character.PRIVATE_USE,
+      Character.SURROGATE,
+      Character.UNASSIGNED,
+      Character.SPACE_SEPARATOR,
+      Character.LINE_SEPARATOR,
+      Character.PARAGRAPH_SEPARATOR,
+    )
 
   def sanitizeSpecializedCharacters(
       string: String,
       stripNewlines: Boolean,
       substituteNonLatin1: Boolean,
   ): String = {
-    var result = string
+    val result: Seq[String] = for (char <- string) yield {
+      char match {
+        // Special case: Special quotes to normal quotes
+        case '‘' => "'"
+        case '‚' => "'"
+        case '’' => "'"
+        case '“' => "\""
+        case '„' => "\""
+        case '”' => "\""
 
-    if (stripNewlines) {
-      result = result.replace("\n", "")
+        // Supported by latin1, but with a different code
+        case '€' => "€"
+        case '‰' => "‰"
+
+        // Invisible characters: Only " " and "\n" are allowed as whitespace
+        case ' '                                                              => " "
+        case '\n'                                                             => if (stripNewlines) "" else "\n"
+        case '\t'                                                             => "  "
+        case _ if INVISIBLE_CHARACTER_TYPES.contains(Character.getType(char)) => ""
+
+        // Substitute non latin1
+        case _ =>
+          if (substituteNonLatin1) {
+            // Strip all unicode characters that are not supported by Latin1
+            val charset = Charset.forName("ISO-8859-1")
+            new String(char.toString.getBytes(charset), charset)
+          } else {
+            char.toString
+          }
+      }
     }
 
-    // Workarounds for operations below that do too much
-    for (char <- EDGE_CASE_REPLACEMENTS.keySet) {
-      result = result.replace(char.toString, EDGE_CASE_REPLACEMENTS.get(char))
-    }
-
-    // Special case: Tab -> "  "
-    result = result.replace("\t", "  ")
-
-    // Special case: Speical quotes to normal quotes
-    result = result.replace("‘", "'")
-    result = result.replace("‚", "'")
-    result = result.replace("’", "'")
-    result = result.replace("“", "\"")
-    result = result.replace("„", "\"")
-    result = result.replace("”", "\"")
-
-    // Strip any kind of whitespace or invisible separator.
-    result = result.replaceAll("[\\p{Z}&&[^ \n]]", "")
-
-    // Strip invisible control characters and unused code points
-    result = result.replaceAll("\\p{C}", "")
-
-    if (substituteNonLatin1) {
-      // Strip all unicode characters that are not supported by Latin1
-      val charset = Charset.forName("ISO-8859-1")
-      result = new String(result.getBytes(charset), charset)
-    }
-
-    // Reverse EDGE_CASE_REPLACEMENTS
-    for (s <- EDGE_CASE_REPLACEMENTS.inverse().keySet) {
-      result = result.replace(s, EDGE_CASE_REPLACEMENTS.inverse().get(s).toString)
-    }
-
-    result
+    result.mkString
   }
 
   def containsSpecialCharacters(
