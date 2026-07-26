79001b9111aea789237fc6f398f75b9d1a64b594
Jens Nyman <nymanjens.nj@gmail.com>
2026-07-26 08:35:00 +0200

    [Bump version to v3.99]

--- app/shared/src/main/scala/app/AppVersion.scala
+++ app/shared/src/main/scala/app/AppVersion.scala
@@ -2,6 +2,6 @@ package app
 
 /** Version string that gets incremented on every deploy. */
 object AppVersion {
-  val versionString: String = "3.98"
+  val versionString: String = "3.99"
   val localDatabaseAndEntityVersion: String = "1.0"
 }
